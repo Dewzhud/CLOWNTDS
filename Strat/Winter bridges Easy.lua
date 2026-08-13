77cdf6fbbb567235490c5f962357a0e8e3043dcd
ZolesAA <117248382+Dewzhud@users.noreply.github.com>
2026-08-13 17:21:34 +0700

    Create Winter bridges Easy.lua

---
+++ Strat/Winter bridges Easy.lua
@@ -0,0 +1,118 @@
+local TDS = loadstring(game:HttpGet("https://raw.githubusercontent.com/DuxiiT/auto-strat/refs/heads/main/Library.lua"))()
+
+TDS:Loadout("Sniper", "Boomerang", "None", "None", "None")
+TDS:Mode("Easy")
+TDS:GameInfo("Winter Bridges", {})
+
+
+-- [ Wave 1 ] --
+TDS:VoteSkip(1)
+TDS:Place("Boomerang", 4.3409576416015625, 1, -0.25801563262939453)
+
+-- [ Wave 2 ] --
+TDS:VoteSkip(2)
+
+-- [ Wave 3 ] --
+TDS:Upgrade(1)
+TDS:VoteSkip(3)
+
+-- [ Wave 4 ] --
+TDS:VoteSkip(4)
+TDS:Upgrade(1)
+
+-- [ Wave 5 ] --
+TDS:Place("Boomerang", 14.987663269042969, 1.000003695487976, -23.06456184387207)
+TDS:SetTarget(2, "Last")
+TDS:SetTarget(2, "Strongest")
+TDS:VoteSkip(5)
+
+-- [ Wave 6 ] --
+TDS:Upgrade(2)
+TDS:VoteSkip(6)
+TDS:Upgrade(2)
+
+-- [ Wave 7 ] --
+TDS:Place("Sniper", -4.348036289215088, 4.19566535949707, -15.463772773742676)
+TDS:Upgrade(3)
+TDS:VoteSkip(7)
+
+-- [ Wave 8 ] --
+TDS:Upgrade(3)
+TDS:Place("Sniper", -3.1785964965820312, 4.1958112716674805, -18.48699188232422)
+TDS:VoteSkip(8)
+TDS:VoteSkip(8)
+TDS:Upgrade(4)
+TDS:Upgrade(4)
+
+-- [ Wave 9 ] --
+TDS:VoteSkip(9)
+TDS:Place("Sniper", -1.5533239841461182, 4.195664882659912, -20.838335037231445)
+
+-- [ Wave 10 ] --
+TDS:Upgrade(5)
+TDS:Upgrade(5)
+TDS:VoteSkip(10)
+
+-- [ Wave 11 ] --
+TDS:VoteSkip(11)
+TDS:Upgrade(2)
+
+-- [ Wave 12 ] --
+TDS:Place("Sniper", -2.8262217044830322, 4.195664882659912, -23.166479110717773)
+TDS:Upgrade(6)
+TDS:Upgrade(6)
+TDS:VoteSkip(12)
+
+-- [ Wave 13 ] --
+TDS:Upgrade(6)
+TDS:Upgrade(5)
+TDS:VoteSkip(13)
+TDS:Upgrade(3)
+
+-- [ Wave 14 ] --
+TDS:VoteSkip(14)
+TDS:Upgrade(4)
+
+-- [ Wave 15 ] --
+TDS:Place("Sniper", -5.220571041107178, 4.860439777374268, -22.750131607055664)
+TDS:Upgrade(7)
+TDS:Upgrade(7)
+TDS:VoteSkip(15)
+TDS:Upgrade(7)
+
+-- [ Wave 16 ] --
+TDS:Upgrade(1)
+TDS:VoteSkip(16)
+
+-- [ Wave 17 ] --
+TDS:Upgrade(7)
+TDS:VoteSkip(17)
+TDS:VoteSkip(17)
+
+-- [ Wave 18 ] --
+TDS:Upgrade(2)
+TDS:VoteSkip(18)
+
+-- [ Wave 19 ] --
+TDS:Upgrade(6)
+TDS:Place("Sniper", -5.23326301574707, 4.860434055328369, -20.196584701538086)
+TDS:Upgrade(8)
+TDS:Upgrade(8)
+TDS:VoteSkip(19)
+
+-- [ Wave 20 ] --
+TDS:Upgrade(8)
+TDS:Upgrade(8)
+TDS:Upgrade(5)
+TDS:Upgrade(4)
+TDS:Upgrade(3)
+TDS:Place("Sniper", -6.899699687957764, 4.19566535949707, -16.257190704345703)
+TDS:Upgrade(9)
+TDS:Upgrade(9)
+TDS:Upgrade(9)
+TDS:Upgrade(9)
+TDS:Place("Sniper", -8.13716983795166, 4.860434055328369, -18.861042022705078)
+TDS:Upgrade(10)
+TDS:Upgrade(10)
+TDS:Upgrade(10)
+TDS:Upgrade(10)
