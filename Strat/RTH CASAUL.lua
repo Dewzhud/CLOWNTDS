999bbf81471e2f399ca168fdc7a3d774ddd79e95
ZolesAA <117248382+Dewzhud@users.noreply.github.com>
2026-08-13 22:57:06 +0700

    Create RTH CASAUL.lua

---
+++ Strat/RTH CASAUL.lua
@@ -0,0 +1,183 @@
+local TDS = loadstring(game:HttpGet("https://raw.githubusercontent.com/DuxiiT/auto-strat/refs/heads/main/Library.lua"))()
+
+TDS:Loadout("Sniper", "Boomerang", "Farm", "Shotgunner", "Slime Trooper")
+TDS:Mode("Casual")
+TDS:GameInfo("Retro The Heights", {})
+
+
+-- [ Wave 1 ] --
+TDS:VoteSkip(1)
+
+-- [ Wave 2 ] --
+TDS:Place("Boomerang", -1.282059669494629, 0.5625, 6.069333076477051)
+TDS:VoteSkip(2)
+
+-- [ Wave 3 ] --
+TDS:Place("Slime Trooper", -5.60599946975708, 1.0625, 18.570661544799805)
+TDS:Upgrade(2)
+
+-- [ Wave 4 ] --
+TDS:Upgrade(1)
+
+-- [ Wave 5 ] --
+TDS:Upgrade(1)
+TDS:Place("Farm", 8.164971351623535, 1.5625, -17.984376907348633)
+TDS:Upgrade(3)
+TDS:Upgrade(2)
+
+-- [ Wave 6 ] --
+TDS:Place("Sniper", 6.657176971435547, 13.0625, 1.1855058670043945)
+TDS:Upgrade(4)
+TDS:VoteSkip(6)
+TDS:SetTarget(2, "Last")
+TDS:SetTarget(2, "Strongest")
+
+-- [ Wave 7 ] --
+TDS:VoteSkip(7)
+TDS:Upgrade(1)
+
+-- [ Wave 8 ] --
+TDS:Upgrade(4)
+TDS:Place("Farm", 3.805631637573242, 1.5625, -17.70416831970215)
+
+-- [ Wave 9 ] --
+TDS:Upgrade(2)
+
+-- [ Wave 10 ] --
+TDS:Upgrade(3)
+TDS:Upgrade(5)
+TDS:Upgrade(5)
+TDS:Upgrade(5)
+TDS:VoteSkip(10)
+
+-- [ Wave 11 ] --
+TDS:Upgrade(4)
+TDS:Place("Sniper", 6.010448455810547, 13.0625, -1.425976276397705)
+TDS:Upgrade(6)
+TDS:Upgrade(6)
+TDS:VoteSkip(11)
+TDS:Upgrade(6)
+
+-- [ Wave 12 ] --
+TDS:VoteSkip(12)
+TDS:Place("Shotgunner", -7.087461471557617, 0.5625, 4.654481410980225)
+TDS:Upgrade(7)
+
+-- [ Wave 13 ] --
+TDS:Upgrade(7)
+TDS:Upgrade(3)
+TDS:VoteSkip(13)
+
+-- [ Wave 14 ] --
+TDS:Place("Farm", -0.07924413681030273, 1.5625, -17.448890686035156)
+TDS:Upgrade(8)
+TDS:Upgrade(8)
+TDS:Upgrade(8)
+TDS:Upgrade(8)
+TDS:Place("Sniper", 6.147524833679199, 13.0625, -4.313786506652832)
+TDS:Upgrade(9)
+TDS:VoteSkip(14)
+TDS:Upgrade(9)
+TDS:Upgrade(9)
+
+-- [ Wave 15 ] --
+TDS:VoteSkip(15)
+TDS:Upgrade(7)
+
+-- [ Wave 16 ] --
+TDS:Upgrade(4)
+TDS:VoteSkip(16)
+
+-- [ Wave 17 ] --
+TDS:Upgrade(2)
+TDS:Place("Boomerang", -6.87007474899292, 1.0625, -1.0989553928375244)
+TDS:Upgrade(10)
+TDS:Upgrade(10)
+TDS:Upgrade(10)
+TDS:VoteSkip(17)
+
+-- [ Wave 18 ] --
+TDS:Upgrade(1)
+TDS:Place("Slime Trooper", -6.079997539520264, 1.0625, 21.812698364257812)
+TDS:Upgrade(11)
+TDS:Upgrade(11)
+TDS:VoteSkip(18)
+TDS:SetTarget(11, "Last")
+TDS:SetTarget(11, "Strongest")
+TDS:Upgrade(11)
+
+-- [ Wave 19 ] --
+TDS:Upgrade(11)
+TDS:Place("Slime Trooper", -9.448873519897461, 1.0625, 13.93122386932373)
+TDS:Upgrade(12)
+TDS:SetTarget(12, "Last")
+TDS:SetTarget(12, "Strongest")
+TDS:Upgrade(12)
+TDS:Upgrade(12)
+TDS:Place("Farm", -3.9968528747558594, 1.5625, -17.524717330932617)
+TDS:Upgrade(13)
+TDS:Upgrade(13)
+TDS:VoteSkip(19)
+
+-- [ Wave 20 ] --
+TDS:Upgrade(12)
+TDS:Upgrade(10)
+TDS:VoteSkip(20)
+
+-- [ Wave 21 ] --
+TDS:Upgrade(9)
+TDS:Place("Sniper", 8.687576293945312, 13.065000534057617, 3.333120346069336)
+TDS:Upgrade(14)
+TDS:Upgrade(14)
+TDS:Upgrade(14)
+TDS:Upgrade(13)
+TDS:VoteSkip(21)
+
+-- [ Wave 22 ] --
+TDS:Place("Shotgunner", -7.224451541900635, 0.5625, 2.0548033714294434)
+TDS:Upgrade(15)
+TDS:Upgrade(15)
+TDS:Upgrade(15)
+TDS:Place("Shotgunner", -5.0947980880737305, 0.5625, 5.148791790008545)
+TDS:Upgrade(16)
+TDS:Upgrade(16)
+TDS:Upgrade(16)
+TDS:Upgrade(13)
+TDS:Upgrade(13)
+TDS:VoteSkip(22)
+TDS:Upgrade(14)
+
+-- [ Wave 23 ] --
+TDS:Place("Sniper", 9.145818710327148, 13.0625, 0.08503484725952148)
+TDS:Upgrade(17)
+TDS:Upgrade(17)
+TDS:Upgrade(17)
+TDS:Upgrade(17)
+TDS:VoteSkip(23)
+
+-- [ Wave 24 ] --
+TDS:Upgrade(16)
+TDS:VoteSkip(24)
+TDS:Upgrade(7)
+
+-- [ Wave 25 ] --
+TDS:Sell(3)
+TDS:Sell(5)
+TDS:Sell(8)
+TDS:Sell(13)
+TDS:Upgrade(15)
+TDS:Place("Sniper", 7.589080810546875, 13.062455177307129, 25.89379119873047)
+TDS:Upgrade(18)
+TDS:Upgrade(18)
+TDS:Upgrade(18)
+TDS:Upgrade(18)
+TDS:Place("Sniper", 11.178962707519531, 13.0625, 2.1342945098876953)
+TDS:Upgrade(19)
+TDS:Upgrade(19)
+TDS:Upgrade(19)
+TDS:Upgrade(19)
+TDS:Place("Shotgunner", -4.635916233062744, 0.5625, 2.2629733085632324)
+TDS:Upgrade(20)
+TDS:Upgrade(20)
+TDS:Upgrade(20)
+TDS:Upgrade(20)
