32fca3198d4116c08e0fbfa1b6e2f9b7c2cc65c9
ZolesAA <117248382+Dewzhud@users.noreply.github.com>
2026-08-14 15:27:31 +0700

    Create RTH E.lua

---
+++ Strat/RTH E.lua
@@ -0,0 +1,118 @@
+local TDS = loadstring(game:HttpGet("https://raw.githubusercontent.com/DuxiiT/auto-strat/refs/heads/main/Library.lua"))()
+
+TDS:Loadout("Sniper", "Boomerang", "None", "None", "None")
+TDS:Mode("Easy")
+TDS:GameInfo("Retro The Heights", {})
+
+TDS:Ready()
+
+-- [ Wave 1 ] --
+TDS:VoteSkip(1)
+
+-- [ Wave 2 ] --
+TDS:Place("Boomerang", -3.9158191680908203, 0.5625, 4.977997779846191)
+TDS:VoteSkip(2)
+
+-- [ Wave 3 ] --
+TDS:Upgrade(1)
+TDS:VoteSkip(3)
+
+-- [ Wave 4 ] --
+TDS:Upgrade(1)
+
+-- [ Wave 5 ] --
+TDS:Place("Boomerang", -5.337675094604492, 1.0625, 22.488330841064453)
+TDS:Upgrade(2)
+TDS:Upgrade(2)
+
+-- [ Wave 6 ] --
+TDS:VoteSkip(6)
+
+-- [ Wave 7 ] --
+TDS:Place("Sniper", 5.872880935668945, 13.0625, 2.68534779548645)
+TDS:Upgrade(3)
+TDS:Upgrade(3)
+TDS:VoteSkip(7)
+
+-- [ Wave 8 ] --
+TDS:Place("Sniper", 5.747577667236328, 13.0625, 0.14157462120056152)
+TDS:Upgrade(4)
+TDS:Upgrade(4)
+TDS:VoteSkip(8)
+
+-- [ Wave 9 ] --
+TDS:Place("Sniper", 5.7955780029296875, 13.0625, -2.558192491531372)
+TDS:Upgrade(5)
+TDS:Upgrade(5)
+TDS:VoteSkip(9)
+
+-- [ Wave 10 ] --
+TDS:Upgrade(2)
+
+-- [ Wave 11 ] --
+TDS:Upgrade(1)
+
+-- [ Wave 12 ] --
+TDS:Upgrade(3)
+TDS:VoteSkip(12)
+
+-- [ Wave 13 ] --
+TDS:Place("Sniper", 8.609952926635742, 13.0625, 2.9225127696990967)
+TDS:Upgrade(6)
+TDS:Upgrade(6)
+TDS:Upgrade(6)
+TDS:Place("Sniper", 8.579368591308594, 13.0625, 0.05843329429626465)
+
+-- [ Wave 14 ] --
+TDS:Upgrade(7)
+TDS:Upgrade(7)
+TDS:Upgrade(7)
+TDS:VoteSkip(14)
+
+-- [ Wave 15 ] --
+TDS:Place("Sniper", 7.711394309997559, 13.0625, -4.697924613952637)
+TDS:Upgrade(8)
+TDS:Upgrade(8)
+TDS:Upgrade(8)
+TDS:VoteSkip(15)
+
+-- [ Wave 16 ] --
+TDS:VoteSkip(16)
+TDS:Place("Sniper", -6.129864692687988, 9.062455177307129, 27.275707244873047)
+TDS:Upgrade(9)
+TDS:Upgrade(9)
+
+-- [ Wave 17 ] --
+TDS:Upgrade(9)
+TDS:Place("Sniper", -6.754618167877197, 9.062455177307129, 24.66814422607422)
+TDS:Upgrade(10)
+TDS:Upgrade(10)
+TDS:Upgrade(10)
+TDS:Place("Sniper", -9.001823425292969, 9.062455177307129, 27.48567008972168)
+TDS:Upgrade(11)
+TDS:Upgrade(11)
+TDS:Place("Sniper", -9.288122177124023, 9.062455177307129, 24.65311050415039)
+TDS:VoteSkip(17)
+TDS:Upgrade(12)
+TDS:Upgrade(12)
+
+-- [ Wave 18 ] --
+TDS:Upgrade(12)
+TDS:Upgrade(4)
+TDS:Upgrade(5)
+TDS:VoteSkip(18)
+
+-- [ Wave 19 ] --
+TDS:Upgrade(3)
+TDS:Upgrade(11)
+TDS:VoteSkip(19)
+
+-- [ Wave 20 ] --
+TDS:Upgrade(9)
+TDS:Upgrade(4)
+TDS:Upgrade(7)
+TDS:Upgrade(11)
+TDS:Upgrade(10)
+TDS:Upgrade(12)
+TDS:Upgrade(5)
+TDS:Upgrade(6)
