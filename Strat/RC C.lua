dd3e20ba54fa24719586339af699df5eb799b7ae
ZolesAA <117248382+Dewzhud@users.noreply.github.com>
2026-08-14 15:31:39 +0700

    Update and rename RC - CASUAL to RC C.lua

--- Strat/RC C.lua
+++ Strat/RC C.lua
@@ -1,6 +1,6 @@
 local TDS = loadstring(game:HttpGet("https://raw.githubusercontent.com/DuxiiT/auto-strat/refs/heads/main/Library.lua"))()
 
-TDS:Loadout("Sniper", "Boomerang", "Farm", "Shotgunner", "Slime Trooper")
+TDS:Loadout("Sniper", "Boomerang", "", "", "")
 TDS:Mode("Casual")
 TDS:GameInfo("Retro Crossroads", {})
 
