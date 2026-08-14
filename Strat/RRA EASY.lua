bd029040b455fb0ae2a138ffb098ff8d3fa39f6b
ZolesAA <117248382+Dewzhud@users.noreply.github.com>
2026-08-14 08:23:44 +0700

    Create RRA EASY.lua

---
+++ Strat/RRA EASY.lua
@@ -0,0 +1,101 @@
+local TDS = loadstring(game:HttpGet("https://raw.githubusercontent.com/DuxiiT/auto-strat/refs/heads/main/Library.lua"))()
+
+TDS:Loadout("Sniper", "Boomerang", "Farm", "Shotgunner", "Slime Trooper")
+TDS:Mode("Easy")
+TDS:GameInfo("Retro Rocket Arena", {})
+
+
+-- [ Wave 1 ] --
+TDS:VoteSkip(1)
+TDS:Place("Boomerang", 3.7689685821533203, 0.5004997253417969, 3.7356112003326416)
+
+-- [ Wave 2 ] --
+TDS:VoteSkip(2)
+
+-- [ Wave 3 ] --
+TDS:Upgrade(1)
+TDS:Upgrade(1)
+
+-- [ Wave 6 ] --
+TDS:VoteSkip(6)
+
+-- [ Wave 7 ] --
+TDS:Upgrade(1)
+TDS:VoteSkip(7)
+
+-- [ Wave 8 ] --
+TDS:VoteSkip(8)
+
+-- [ Wave 9 ] --
+TDS:VoteSkip(9)
+
+-- [ Wave 10 ] --
+TDS:VoteSkip(10)
+TDS:Place("Boomerang", -3.3397488594055176, 0.5004997253417969, -5.002802848815918)
+TDS:Upgrade(2)
+TDS:Upgrade(2)
+
+-- [ Wave 11 ] --
+TDS:Place("Sniper", -1.9881258010864258, 4.25, -7.776277542114258)
+TDS:Upgrade(3)
+TDS:Upgrade(3)
+TDS:Upgrade(3)
+TDS:VoteSkip(11)
+
+-- [ Wave 12 ] --
+TDS:Place("Sniper", 7.865351676940918, 4.25, -2.1064324378967285)
+TDS:Upgrade(4)
+TDS:Upgrade(4)
+TDS:Upgrade(4)
+TDS:VoteSkip(12)
+
+-- [ Wave 13 ] --
+TDS:Upgrade(2)
+TDS:VoteSkip(13)
+
+-- [ Wave 14 ] --
+TDS:Place("Sniper", 2.390474319458008, 4.25, 7.6959991455078125)
+TDS:Upgrade(5)
+TDS:Upgrade(5)
+TDS:Upgrade(5)
+TDS:VoteSkip(14)
+
+-- [ Wave 15 ] --
+TDS:Place("Sniper", -2.3020358085632324, 4.25, 7.482365608215332)
+TDS:Upgrade(6)
+TDS:Upgrade(6)
+TDS:Upgrade(6)
+TDS:VoteSkip(15)
+
+-- [ Wave 16 ] --
+TDS:Place("Sniper", 1.7651333808898926, 4.25, -7.647065162658691)
+TDS:Upgrade(7)
+TDS:Upgrade(7)
+TDS:Upgrade(7)
+TDS:VoteSkip(16)
+
+-- [ Wave 17 ] --
+TDS:Upgrade(4)
+TDS:VoteSkip(17)
+
+-- [ Wave 18 ] --
+TDS:Upgrade(7)
+TDS:VoteSkip(18)
+TDS:Upgrade(3)
+
+-- [ Wave 19 ] --
+TDS:Place("Sniper", 7.298961639404297, 4.25, 1.9815419912338257)
+TDS:Upgrade(8)
+TDS:Upgrade(8)
+TDS:Upgrade(8)
+TDS:VoteSkip(19)
+
+-- [ Wave 20 ] --
+TDS:Upgrade(8)
+TDS:Place("Sniper", -7.328063488006592, 4.25, -2.5)
+TDS:Upgrade(9)
+TDS:Upgrade(9)
+TDS:Upgrade(9)
+TDS:Upgrade(9)
+TDS:Upgrade(5)
+TDS:Upgrade(6)
